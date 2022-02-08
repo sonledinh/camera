4a1c90e5f500c9ae7e5324cbdf0bf5bcd76aa1b7
sonledinh <sonledinh@admicro.vn>
2022-02-09 01:21:57 +0700

    demo

---
+++ header.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Tech</title>
+    <!--link css-->
+    <link rel="stylesheet" type="text/css" title="" href="css/lib/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" title="" href="css/font-awesome.css">
+    <link rel="stylesheet" type="text/css" title="" href="css/lib/slick.min.css">
+    <link rel="stylesheet" type="text/css" title="" href="css/lib/slick-theme.min.css">
+    <link rel="stylesheet" type="text/css" title="" href="css/lib/jquery.mmenu.all.css">
+    <link rel="stylesheet" type="text/css" title="" href="css/style.css">
+
+    <script type="text/javascript" src="js/lib/jquery.min.js"></script> 
+</head>
+<body>  
+    <header>
+        <div class="header-pc">
+            <div class="container-fluid">
+                <div class="content-head">
+                    <div class="left">
+                        <div class="h-menu">
+                            <ul>
+                                <li><a href=""><img src="images/logo.svg" class="img-fluid" alt=""></a></li>
+                                <li><a href="">Sản phẩm</a></li>
+                                <li><a href="">Giải pháp</a></li>
+                                <li><a href="">Dự án</a></li>
+                                <li><a href="">Tin tức</a></li>
+                                <li><a href="">Hỗ trợ</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="right">
+                        <div class="h-right">
+                            <ul>
+                                <li><a href="" class="btn-head">Trở thành Đại lý</a></li>
+                                <li>
+                                    <a href="" class="ac-head">
+                                        <svg width="19" height="20" viewBox="0 0 19 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <circle cx="8.437" cy="8.50878" r="6.96239" stroke="#100A0A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                            <path d="M13.4023 13.8936L17.591 18.8683" stroke="#100A0A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                        </svg>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="" class="ac-head">
+                                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path fill-rule="evenodd" clip-rule="evenodd" d="M7.5345 23.1247C8.50144 23.1247 9.28531 22.3408 9.28531 21.3739C9.28531 20.4069 8.50144 19.623 7.5345 19.623C6.56755 19.623 5.78369 20.4069 5.78369 21.3739C5.78369 22.3408 6.56755 23.1247 7.5345 23.1247Z" stroke="#100A0A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                            <path fill-rule="evenodd" clip-rule="evenodd" d="M18.0394 23.1247C19.0063 23.1247 19.7902 22.3408 19.7902 21.3739C19.7902 20.4069 19.0063 19.623 18.0394 19.623C17.0724 19.623 16.2886 20.4069 16.2886 21.3739C16.2886 22.3408 17.0724 23.1247 18.0394 23.1247Z" stroke="#100A0A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                            <path d="M5.49487 8.24274H20.6656L19.1949 15.5874C19.0297 16.4192 18.2921 17.013 17.4441 16.9968H8.19112C7.3065 17.0043 6.55528 16.3507 6.44031 15.4736L5.1097 5.38892C4.99556 4.51842 4.25435 3.86705 3.3764 3.86572H1.40674" stroke="#100A0A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                        </svg>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="" class="ac-head">
+                                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M20 21V19C20 16.7909 18.2091 15 16 15H8C5.79086 15 4 16.7909 4 19V21" stroke="#100A0A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                            <path fill-rule="evenodd" clip-rule="evenodd" d="M12 11C14.2091 11 16 9.20914 16 7C16 4.79086 14.2091 3 12 3C9.79086 3 8 4.79086 8 7C8 9.20914 9.79086 11 12 11Z" stroke="#100A0A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                        </svg>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+ 
